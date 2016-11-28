be9e4ad2d64e8195fddfb5c31be5692d9a6afd29
Brandon Padgett <gerane@gmail.com>
2016-11-28 15:53:13 -0600

    Updated outfile

--- Update-VSCodeSnippets.ps1
+++ Update-VSCodeSnippets.ps1
@@ -22,46 +22,53 @@ function Update-VSCodeSnippets
 
     Process
     {
-        $JsonPath = "$ENV:AppData\$CodeDir\User\snippets\$Language.json"
-        if (Test-Path $Path)
+        Try
         {
-            $CurrentSnippets = Get-Content -Path $JsonPath -Raw | ConvertFrom-Json
-        }
-        else
-        {
-            $Null = New-Item -Path $JsonPath
-            $CurrentSnippets = @{}
-        }
-
-        $SnippetsMetadata = Get-ChildItem -Path $PSScriptRoot\Metadata
-
-        foreach ($Metadata in $SnippetsMetadata)
-        {
-            $SnippetName = [io.path]::GetFileNameWithoutExtension($($Metadata.Name))
-            $Snippet = . $Metadata.Fullname
-
-            if (!$CurrentSnippets.$SnippetName)
+            $JsonPath = "$ENV:AppData\$CodeDir\User\snippets\$Language.json"
+            if (Test-Path $JsonPath)
             {
-                $CurrentSnippets | Add-Member -MemberType NoteProperty -Name $SnippetName -Value $Snippet
+                $CurrentSnippets = Get-Content -Path $JsonPath -Raw | ConvertFrom-Json
             }
             else
             {
-                if ($CurrentSnippets.$SnippetName.prefix -ne $Snippet.prefix)
-                {
-                    $CurrentSnippets.$SnippetName.prefix = $Snippet.prefix
-                }
-
-                if ($CurrentSnippets.$SnippetName.description -ne $Snippet.description)
-                {
-                    $CurrentSnippets.$SnippetName.description = $Snippet.description
-                }
-
-                if ($CurrentSnippets.$SnippetName.body -ne $Snippet.body)
-                {
-                    $CurrentSnippets.$SnippetName.body = $Snippet.body
-                }
+                $Null = New-Item -Path $JsonPath
+                $CurrentSnippets = @{}
             }
-            ($CurrentSnippets) | ConvertTo-Json | Out-File C:\TestPowerShell.json -Encoding ascii -force
+
+            $SnippetsMetadata = Get-ChildItem -Path $PSScriptRoot\Metadata
+
+            foreach ($Metadata in $SnippetsMetadata)
+            {
+                $SnippetName = [io.path]::GetFileNameWithoutExtension($($Metadata.Name))
+                $Snippet = . $Metadata.Fullname
+
+                if (!$CurrentSnippets.$SnippetName)
+                {
+                    $CurrentSnippets | Add-Member -MemberType NoteProperty -Name $SnippetName -Value $Snippet
+                }
+                else
+                {
+                    if ($CurrentSnippets.$SnippetName.prefix -ne $Snippet.prefix)
+                    {
+                        $CurrentSnippets.$SnippetName.prefix = $Snippet.prefix
+                    }
+
+                    if ($CurrentSnippets.$SnippetName.description -ne $Snippet.description)
+                    {
+                        $CurrentSnippets.$SnippetName.description = $Snippet.description
+                    }
+
+                    if ($CurrentSnippets.$SnippetName.body -ne $Snippet.body)
+                    {
+                        $CurrentSnippets.$SnippetName.body = $Snippet.body
+                    }
+                }
+                ($CurrentSnippets) | ConvertTo-Json | Out-File $JsonPath -Encoding ascii -force
+            }
+        }
+        Catch
+        {
+            Throw
         }
     }
 }
